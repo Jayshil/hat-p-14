7eddd44bbeca8674807a3a623240eb272a13bd58
Jayshil <jayshilpatel147@gmail.com>
2023-10-30 15:43:56 +0100

    refining the FWHM calculation

--- NRCLW/plot1.py
+++ NRCLW/plot1.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import pickle
 import os
 import utils
+import astropy.units as u
+from astropy.timeseries import LombScargle
 
 # This file is to visualise how FWHM varies with time
 
@@ -33,7 +35,10 @@ for i in range(len(segs)-1):
     time_seg = np.load(pin2 + '/Times_bjd_seg' + segs[i+1] + '.npy')
     times = np.hstack((times, time_seg))
 
-med_fwhm = np.nanmedian(fwhm, axis=1)
+med_fwhm_along_time = np.nanmedian(fwhm, axis=0)
+norm_fwhm = fwhm / med_fwhm_along_time[None,:]
+
+med_fwhm = np.nanmedian(norm_fwhm, axis=1)
 times = times + 2400000.5# - 2459702.
 
 # For FWHM
@@ -48,7 +53,7 @@ tbin_sf2, fbin_sf2, flebin_sf2, _ = utils.lcbin(time=times, flux=super_fwhm, bin
 fig, ax = plt.subplots(figsize=(16/1.5,9/1.5))
 
 # Top panel
-ax.errorbar(times, med_fwhm, fmt='.', c='cornflowerblue', alpha=0.5)
+ax.errorbar(times, med_fwhm, fmt='-', c='cornflowerblue', alpha=0.5)
 ax.errorbar(tbin, fbin, yerr=febin, fmt='.', c='gray', alpha=0.7, zorder=50)
 ax.errorbar(tbin2, fbin2, yerr=febin2, fmt='o', c='navy', elinewidth=2, capthick=2, capsize=3, mfc='white', zorder=150)
 ax.set_ylabel('FWHM', fontsize=14)
@@ -60,6 +65,60 @@ plt.setp(ax.get_yticklabels(), fontsize=12)
 ax.set_title('FWHM for Visit ' + str(visit), fontsize=15)
 #plt.show()
 plt.savefig(pin + '/Figures/Full_fwhm_' + visit + '.png', dpi=500)
+
+
+# Finding PSD of the FWHM
+def find_PSD(time, data):
+    tim1 = time*24*60*60*u.second
+
+    min_frequency = 1. / (np.ptp(tim1))   # inverse clock-time of a column
+    max_frequency = (1. / (tim1[1]-tim1[0])) * 0.5
+    freq = np.linspace(min_frequency, max_frequency, 100000)
+
+    comps = 1e6*data*u.dimensionless_unscaled
+    power = LombScargle(tim1,comps).power(frequency=freq)
+
+    freq1 = freq[np.argsort(power.value)[-1]]
+    per2 = 1/freq1
+    per3 = per2.to(u.day)
+    max_power_loc_min = per3.to(u.min)
+
+    return freq, power, max_power_loc_min
+
+def freq2min(x):
+    x = 1/x * u.s
+    return x.to(u.min).value
+
+def min2freq(x):
+    x = x*u.min
+    x = 1/x
+    return x.to(u.s**-1).value
+
+freq, power, max_power = find_PSD(times, med_fwhm)
+print('>>> --- Maximum power at {:.4f}'.format(max_power))
+
+# Making the plot:
+fig, axs = plt.subplots(figsize=(16/1.5, 9/1.5))
+
+axs.plot(freq.value, power.value, color='orangered', alpha=0.9, label='PC 4')
+#axs.axvline(freq1.value, ls='--', c='darkgreen')
+axs.set_xscale('log')
+axs.set_yscale('log')
+
+axs.legend(loc='best')
+
+axs.set_xlabel(r'Frequency [Hz]')
+axs.set_ylabel(r'Power [ppm$^2$ Hz$^{-1}$] + Offset')
+#axs.set_title('PSD of the PCA component' + str(nComp) + ' (Visit ' + visit[-1] + ')')
+
+secax = axs.secondary_xaxis('top', functions=(freq2min, min2freq))
+secax.set_xlabel(r'Time [min]')
+
+axs.set_xlim([np.min(freq.value), np.max(freq.value)])
+#plt.show()
+plt.savefig(pin + '/Figures/Fwhm_PSD_' + visit + '.png', dpi=500)
+
+# Super FWHM
 
 # For Full model
 fig, ax = plt.subplots(figsize=(16/1.5,9/1.5))
